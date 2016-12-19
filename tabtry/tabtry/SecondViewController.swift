71a31b6da7bc015a8374f61c6e5c8325b8c4d7c9
ysyucel <ysyucel@icloud.com>
2016-12-19 11:44:13 +0200

    start prroject
    tab try basic

---
+++ tabtry/tabtry/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabtry
+//
+//  Created by Developer on 19.12.2016.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
